bafd9c8198837e3d362f636e1963a0babc6b980e
Douglas Barreto <doouglas_barreto@hotmail.com>
2024-07-13 18:17:39 -0300

    Criação TripsController
    Foi criado a classe TripsController e a Classe RegisterTripUseCase para trabalhar as rotinas de registros do Contorller para Viagens.

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute(RequestRegisterTripJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestRegisterTripJson request)
+        {
+            if (string.IsNullOrEmpty(request.Name))
+            {
+                throw new ArgumentException("Nome não pode ser vazio");
+            }
+
+            if (request.StartDate < DateTime.UtcNow)
+            {
+                throw new ArgumentException("Data de início não pode ser menor que a data atual");
+            }
+
+            if (request.EndDate < request.StartDate)
+            {
+                throw new ArgumentException("Data de término não pode ser menor que a data de início");
+            }
+        }
+
+        
+    }
+}
